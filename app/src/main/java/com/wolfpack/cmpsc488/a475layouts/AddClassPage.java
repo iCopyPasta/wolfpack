fff1e4e82eaa96162bf16d8162263ed4152e99b6
Tyler Ruch <sacapuntas9@gmail.com>
2018-01-29 23:45:40 -0500

    android layouts

---
+++ app/src/main/java/com/wolfpack/cmpsc488/a475layouts/AddClassPage.java
@@ -0,0 +1,13 @@
+package com.wolfpack.cmpsc488.a475layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddClassPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_class_page);
+    }
+}
---
+++ app/src/main/java/com/wolfpack/cmpsc488/a475layouts/StudentPage.java
@@ -0,0 +1,13 @@
+package com.wolfpack.cmpsc488.a475layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_page);
+    }
+}
