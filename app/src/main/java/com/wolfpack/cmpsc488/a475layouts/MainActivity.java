31e573eb4afa1d973f993334a03558bc82c593df
Pablo Orellana <thepabloski@gmail.com>
2018-02-09 20:58:42 -0500

    create basic login page and link to a dummy authentication

--- app/src/main/java/com/wolfpack/cmpsc488/a475layouts/MainActivity.java
+++ app/src/main/java/com/wolfpack/cmpsc488/a475layouts/MainActivity.java
@@ -1,7 +1,9 @@
 package com.wolfpack.cmpsc488.a475layouts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO: Add logic in order to skip MainActivity if the user already authenticated
     }
+
+    protected void onPause() {
+        super.onPause();
+    }
+
+    protected void onStop() {
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    public void onLogin(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
