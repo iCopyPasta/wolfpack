76906fc3d8b4c12744ee0e99e1c799bcd35424a9
Scott Wilson <kandabar@hotmail.com>
2018-03-01 23:33:45 -0500

    using Connection.php
    still need to figure out how to insert the json object

--- pages/Create_Class_teacher.php
+++ pages/Create_Class_teacher.php
@@ -1,26 +1,23 @@
 <?php
-  $host = 'localhost';
-  $user = 'root';
-  $password = '';
-  $dbname = 'iClicker';
-
-  // Set DSN
-  $dsn = 'mysql:host='.$host.';dbname='.$dbname;
-
-  // Create a PDO instance
-  $pdo = new PDO($dsn, $user, $password);
-
   // PDO INSERT Statement
-  $class_course_number = $_POST['class_course_number'];
-  $location = $_POST['location'];
-  $offering = $_POST['offering'];
+  include('Connection.php');
+  $pdo = new Connection;
 
-  $sql = "INSERT INTO class_course
-                          (class_course_number, location, offering)
-                          VALUES (:class_course_number, :location, :offering)";
+  if (!isset($myObj)) $myObj = new stdClass();
+  $myObj->success = false;
+  $myObj->class_course_number = $_POST['class_course_number'];
+  $myObj->location = $_POST['location'];
+  $myObj->offering = $_POST['offering'];
+  $myJSON = json_encode($myObj);
 
-  $stmt = $pdo->prepare($sql);
-  $stmt->execute(['class_course_number' => $class_course_number, 'location' => $location, 'offering' => $offering]);
-  echo 'Post Added <br>'.$class_course_number.' '.$location.' '.$offering;
+  echo $myJSON;
+
+  // $sql = "INSERT INTO class_course
+  //                         (class_course_number, location, offering)
+  //                         VALUES (:class_course_number, :location, :offering)";
+  //
+  // $stmt = $pdo->prepare($sql);
+  // $stmt->execute(['class_course_number' => $class_course_number, 'location' => $location, 'offering' => $offering]);
+  // echo 'Post Added <br>'.$class_course_number.' '.$location.' '.$offering;
 
 ?>
