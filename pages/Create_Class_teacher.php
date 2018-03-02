fffdd931e7cc661001847ba4b78a81f04634df01
Scott Wilson <kandabar@hotmail.com>
2018-03-02 14:42:42 -0500

    new Class "ClassCourse"

--- pages/Create_Class_teacher.php
+++ pages/Create_Class_teacher.php
@@ -1,23 +1,69 @@
 <?php
-  // PDO INSERT Statement
-  include('Connection.php');
-  $pdo = new Connection;
+  /*
+  This class is used by the teacher to insert a new class into table Class_Course.
 
-  if (!isset($myObj)) $myObj = new stdClass();
-  $myObj->success = false;
-  $myObj->class_course_number = $_POST['class_course_number'];
-  $myObj->location = $_POST['location'];
-  $myObj->offering = $_POST['offering'];
-  $myJSON = json_encode($myObj);
+  Example Usage:
+  include('Create_Class_teacher.php');
+  $insertClass = new ClassCourse(460, 'Olmstead', 'what is offering?');
+  $insertClass->insert();
+  */
 
-  echo $myJSON;
+  class ClassCourse{
+    private $class_course_number;
+    private $location;
+    private $offering;
 
-  // $sql = "INSERT INTO class_course
-  //                         (class_course_number, location, offering)
-  //                         VALUES (:class_course_number, :location, :offering)";
-  //
-  // $stmt = $pdo->prepare($sql);
-  // $stmt->execute(['class_course_number' => $class_course_number, 'location' => $location, 'offering' => $offering]);
-  // echo 'Post Added <br>'.$class_course_number.' '.$location.' '.$offering;
+    function __construct($c,$l,$o) {
+          $this->__set('class_course_number', $c);
+          $this->__set('location',$l);
+          $this->__set('offering',$o);
+        }
 
+    // magical get
+    // reference: http://php.net/manual/en/language.oop5.magic.php
+    public function __get($property) {
+      if (property_exists($this, $property)) {
+        return $this->$property;
+      }
+    }
+
+    // magical set
+    // reference: http://php.net/manual/en/language.oop5.magic.php
+    public function __set($property, $value) {
+      if (property_exists($this, $property)) {
+        $this->$property = $value;
+      }
+      return $this;
+    }
+
+    public function insert(){
+      include('Connection.php');
+      $connection = new Connection;
+      $pdo = $connection->getConnection();
+
+      $sql = "INSERT INTO class_course
+                              (class_course_number, location, offering)
+                              VALUES (:class_course_number, :location, :offering)";
+      $stmt = $pdo->prepare($sql);
+
+      try{
+        $stmt->execute(['class_course_number' => $this->class_course_number, 'location' => $this->location, 'offering' => $this->offering]);
+      }catch (Exception $e){
+        // fail JSON response
+        $response = array();
+        $response["message"] = "ERROR on INSERT: " . $e->getMessage();
+        $response["success"] = 0;
+        echo json_encode($response);
+      }
+
+      // success JSON response
+      $response = array();
+      $response["message"] = "Inserted: ";
+      $response["success"] = 1;
+      echo json_encode($response);
+
+      $pdo = null;
+    }
+
+  }
 ?>
