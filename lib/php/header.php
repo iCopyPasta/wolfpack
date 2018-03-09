282e16ff15a84e7a050af4fa34f7999bdb0eba06
TheRealPrestige <Fenikkusu712@gmail.com>
2018-03-08 20:40:34 -0500

    Revert "Merge branch 'TeacherLandingPage' into Web"
    This reverts commit f3df9c9bc6e1c7ff4cdb615f3e7606469ee1317f. Partially

--- lib/php/header.php
+++ lib/php/header.php
@@ -17,8 +17,8 @@
         <a class="nav-link disabled" href="#">Disabled</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="nav-link dropdown-toggle" href="index.php" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown01">
           <a class="dropdown-item" href="#">Action</a>
           <a class="dropdown-item" href="#">Another action</a>
           <div class="dropdown-divider"></div>
--- pages/Sign_in_student.php
+++ pages/Sign_in_student.php
@@ -119,8 +119,7 @@ body {
   <body class="text-center">
 
 <?php include("../lib/php/header.php"); ?>
-<?php include("../lib/php/header.php"); ?>
-  
+
     <form class="form-signin" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
       <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
