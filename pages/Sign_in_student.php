b4fe4631e4c876df7d24d3bd9aaae4b390ecc100
TheRealPrestige <Fenikkusu712@gmail.com>
2018-03-08 20:27:05 -0500

    Try again

--- pages/Sign_in_student.php
+++ pages/Sign_in_student.php
@@ -121,8 +121,6 @@ body {
   <body class="text-center">
 
 <?php include("../lib/php/header.php"); ?>
-
-  
 <?php include("../lib/php/header.php"); ?>
   
     <form class="form-signin" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
