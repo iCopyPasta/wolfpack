06cf499713fc263661dc828c94ba331a8754ccba
Scott Wilson <kandabar@hotmail.com>
2018-03-12 00:18:39 -0400

    oopsies

--- pages/Connection.php
+++ pages/Connection.php
@@ -1,62 +1,63 @@
 <?php
-/*
-* Generic class to allow logins.
-*/
-class Connection{
+  /*
+  * Generic class to allow logins.
+  */
+  class Connection{
 
     function getConnection(){
+
       $host = 'localhost';
-      $username = 'root';
-      $password = '';
+      $username = 'wolfpack_tester';
+      $password = 'cashRule$Everything101';
       $dbname = 'wolfpack_tester';
 
-	    try{
-          $conn    = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          return $conn;
+      try{
+        $conn    = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
 
-        }catch (PDOException $e){
-    	    //may wish to port output elsewhere
-          $response = array();
-    	    $response["message"] = "ERROR CONNECTION FAILURE: " . $e->getMessage();
-          $response["success"] = 0;
-    	    echo json_encode($response);
-        }
+      }catch (PDOException $e){
+        //may wish to port output elsewhere
+        $response = array();
+        $response["message"] = "ERROR CONNECTION FAILURE: " . $e->getMessage();
+        $response["success"] = 0;
+        echo json_encode($response);
+      }
 
     }
 
 
     function getConnectionArgs($hostArg, $usernameArg, $passwordArg, $dbnameArg){
 
-    	if($hostArg !== null){
-	        $host = $hostArg;
-	    }
+      if($hostArg !== null){
+        $host = $hostArg;
+      }
 
-	    if($usernameArg !== null){
-	        $username = $usernameArg;
-	    }
+      if($usernameArg !== null){
+        $username = $usernameArg;
+      }
 
-	    if($passwordArg !== null){
-	        $password = passwordArg;
-	    }
+      if($passwordArg !== null){
+        $password = passwordArg;
+      }
 
-	    if($dbnameArg !== null){
-	        $dbname = $dbnameArg;
-	    }
+      if($dbnameArg !== null){
+        $dbname = $dbnameArg;
+      }
 
 
-        try{
-          $conn    = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          return $conn;
+      try{
+        $conn    = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
 
-        }catch (PDOException $e){
-          //may wish to port output elsewhere
-          $response = array();
-          $response["message"] = "ERROR CONNECTION FAILURE: " . $e->getMessage();
-          $response["success"] = 0;
-          echo json_encode($response);
-        }
+      }catch (PDOException $e){
+        //may wish to port output elsewhere
+        $response = array();
+        $response["message"] = "ERROR CONNECTION FAILURE: " . $e->getMessage();
+        $response["success"] = 0;
+        echo json_encode($response);
+      }
     }
-}
+  }
 ?>
