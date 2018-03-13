6f60a571be097894d92cdf888c04809ce6e8baef
Tyler <sacapuntas9@gmail.com>
2018-03-13 02:03:27 -0400

    Added session functionality for student. Upon successful login, a php session is created, storing the email in a non-client accessible way.
    header.php displays login status and can be used as a template for using the session functionality.

--- lib/php/header_index.php
+++ lib/php/header_index.php
@@ -1,6 +1,9 @@
+<?php //include this code at the top of all pages with special functionality for a logged in user, or put it in header
+ session_start(); ?>
+
 <!-- Toolbar currently does nothing, just a default placeholder -->
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-  <a class="navbar-brand" href="index.php">Pollato</a>
+  <a class="navbar-brand" href="../index.php">Pollato</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -17,14 +20,25 @@
         <a class="nav-link disabled" href="#">Disabled</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="index.php" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-        <div class="dropdown-menu" aria-labelledby="dropdown01">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="#">Action</a>
           <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#">Something else here</a>
         </div>
       </li>
+        
     </ul>
-
+      <p style="color:white">
+    <?php if (isset($_SESSION['user'])) {
+        // logged in
+        echo "Hello ".$_SESSION['user'];
+        } else {
+        // not logged in
+        echo "Not logged in.";
+        }
+      ?>
+      </p>
   </div>
 </nav>
