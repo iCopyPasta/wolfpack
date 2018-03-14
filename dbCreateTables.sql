90d0068889d0d14f52aca27d96158833da2cfbed
Donutts <kandabar@hotmail.com>
2018-03-14 19:35:30 -0400

    update dbCreateTables.sql for professor_account

--- dbCreateTables.sql
+++ dbCreateTables.sql
@@ -91,13 +91,16 @@ CREATE TABLE IF NOT EXISTS `professor_account` (
   `professor_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `first_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `last_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `professor_school_id` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `student_school_id` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `salted_password` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `uniqueID` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `isConfirmed` tinyint(1) DEFAULT '0',
+  `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`professor_id`),
   UNIQUE KEY `professor_id` (`professor_id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `question` (
   `question_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
