61e9315cdd73baa468aff179cd43ebf281ee1973
Pablo Orellana <peo5032@psu.edu>
2018-03-14 02:27:15 -0400

    add TODO for service that will continually ask in a background thread

---
+++ app/src/main/java/com/wolfpack/cmpsc488/a475layouts/services/pollingsession/MyIntentService.java
@@ -0,0 +1,78 @@
+package com.wolfpack.cmpsc488.a475layouts.services.pollingsession;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class MyIntentService extends IntentService {
+    // TODO: Rename actions, choose action names that describe tasks that this
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_FOO = "com.wolfpack.cmpsc488.a475layouts.services.pollingsession.action.FOO";
+
+    // TODO: Rename parameters
+    private static final String EXTRA_PARAM1 = "com.wolfpack.cmpsc488.a475layouts.services.pollingsession.extra.PARAM1";
+    private static final String EXTRA_PARAM2 = "com.wolfpack.cmpsc488.a475layouts.services.pollingsession.extra.PARAM2";
+
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionFoo(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, MyIntentService.class);
+        intent.setAction(ACTION_FOO);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    /**
+     * Starts this service to perform action Baz with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_FOO.equals(action)) {
+                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
+                handleActionFoo(param1, param2);
+            }
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionFoo(String param1, String param2) {
+        // TODO: Handle action Foo
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    //TODO: incoporate network request in service
+    //TODO: incorporate service into android manifest
+    //TODO: incorporate retrofit call to particular file with values passed via intent
+    //TODO: incoporate broadcast manager to send message of existing poll
+    //TODO: incoporate broadcast listener + intent filter in main fragment
+    
+
+}
