b609175efd21d8892e7585098359d91839a2646e
Donutts <kandabar@hotmail.com>
2018-03-15 03:08:00 -0400

    Update insertDataIntoProfessorAccount.sql

--- insertDataIntoProfessorAccount.sql
+++ insertDataIntoProfessorAccount.sql
@@ -1,16 +1,23 @@
 DELETE FROM professor_account;
 ALTER TABLE professor_account AUTO_INCREMENT = 1;
-CREATE TABLE IF NOT EXISTS `professor_account` (
-  `professor_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `last_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `professor_school_id` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `salted_password` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `uniqueID` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `isConfirmed` tinyint(1) DEFAULT '0',
-  `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`professor_id`),
-  UNIQUE KEY `professor_id` (`professor_id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+INSERT INTO `professor_account` (`professor_id`, `first_name`, `last_name`, `professor_school_id`, `email`, `salted_password`, `uniqueID`, `isConfirmed`, `title`) VALUES
+(1, 'Mitchell', 'Garza', '1406', 'qrczak@msn.com', 'X27I8IvquQ2tXQrr3c9M', '9687', 0, 'Mr.'),
+(2, 'Maxim', 'Glover', '1407', 'yzheng@live.com', 'iD3HpXZezjcpEc0yxnI1', '9422', 0, 'Mrs.'),
+(3, 'Mackenzie', 'Sherman', '4713', 'benits@gmail.com', 'RSuvdJNB54T9iSgpfcbV', '9642', 1, 'Mr.'),
+(4, 'Adalyn', 'Robinson', '2320', 'scarlet@sbcglobal.net', 'gLhsGTY7XLAl9y0aU0FR', '9232', 1, 'Miss'),
+(5, 'Isla', 'Yu', '4728', 'feamster@live.com', '831rjcGvPLkbl1ZN89Wa', '9036', 1, 'Mr.'),
+(6, 'Jasper', 'Glenn', '1406', 'studyabr@aol.com', 'iDlK4YwOkq3jg2iLWiQ2', '7100', 0, 'Mr.'),
+(7, 'Jaron', 'Moon', '1407', 'teverett@icloud.com', '97tiaw1zyoFZcHoL0mnV', '7733', 0, 'Mrs.'),
+(8, 'Kamron', 'Rogers', '4713', 'mccurley@verizon.net', '6jxgK3YgPX3mbN3Euzr4', '9575', 0, 'Mr.'),
+(9, 'Heath', 'Chung', '2320', 'cliffordj@msn.com', 'MnxU2MxRCA98Sfitv6Tk', '5988', 1, 'Miss'),
+(10, 'Susan', 'Dominguez', '4728', 'cgarcia@aol.com', 'y21D4TImURHataYUxa57', '6618', 0, 'Mr.'),
+(11, 'Brenton', 'Clay', '1406', 'hmbrand@icloud.com', 'sCcM1M3FMj15mn0quZj7', '5845', 1, 'Mr.'),
+(12, 'Shea', 'Singleton', '1407', 'parasite@comcast.net', 'aD1xb4yTdWaTkMdESzSu', '6634', 1, 'Mrs.'),
+(13, 'Dylan', 'Odonnell', '4713', 'lridener@optonline.net', 'pW6QBLWDZwYxaoN5L5uv', '9248', 1, 'Mr.'),
+(14, 'Blake', 'Reed', '2320', 'jorgb@icloud.com', 'K6KNvIvSxfrDXxd8Z0hH', '5250', 0, 'Miss'),
+(15, 'Jacquelyn', 'Downs', '4728', 'kalpol@mac.com', 'G6zO7PqXQrY7oWQlOqdb', '8097', 0, 'Mr.'),
+(16, 'Hugh', 'Forbes', '1406', 'nwiger@hotmail.com', 'lsUj7viOxS6DdZfc2FkZ', '7891', 1, 'Mr.'),
+(17, 'Lewis', 'Orr', '1407', 'rsmartin@gmail.com', 'X52DJUQEg0beOwjfwCUc', '9536', 0, 'Mrs.'),
+(18, 'Kole', 'Holloway', '4713', 'wenzlaff@sbcglobal.net', 'DVCUdptwnsZDiA1kvnpO', '8492', 0, 'Mr.'),
+(19, 'Ximena', 'Singh', '2320', 'mrdvt@me.com', 'MRvsLCSSi9nOgXdgvAtr', '5162', 0, 'Miss'),
+(20, 'Andres', 'Osborn', '4728', 'lamprecht@gmail.com', 'Mdx3MQvD5fP2dmz39tOI', '5439', 1, 'Mr.');
