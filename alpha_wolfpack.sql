623a176030faa27a1271ca7f9e7b7fd1d7797d41
Tyler Ruch <sacapuntas9@gmail.com>
2018-03-16 16:39:36 -0400

    add foreign key constraints

--- alpha_wolfpack.sql
+++ alpha_wolfpack.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 16, 2018 at 08:52 AM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Host: localhost
+-- Generation Time: Mar 16, 2018 at 04:38 PM
+-- Server version: 5.7.21-0ubuntu0.16.04.1
+-- PHP Version: 7.0.25-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -53,25 +53,12 @@ CREATE TABLE `active_question_set` (
 DROP TABLE IF EXISTS `answers`;
 CREATE TABLE `answers` (
   `student_id` bigint(20) NOT NULL,
-  `sesssion_id` bigint(20) NOT NULL,
+  `session_id` bigint(20) NOT NULL,
   `question_history_id` bigint(20) NOT NULL,
   `answer_type` varchar(200) DEFAULT NULL,
   `answer` blob,
   `submit_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `answers`
---
-
-INSERT INTO `answers` (`student_id`, `sesssion_id`, `question_history_id`, `answer_type`, `answer`, `submit_time`) VALUES
-(1, 1, 1, 'multiple_choice', NULL, '2018-03-16 04:00:10'),
-(2, 1, 1, 'multiple_choice', NULL, '2018-03-16 08:38:43'),
-(3, 2, 4, 'multiple_choice', NULL, '2018-03-16 08:49:10'),
-(1, 2, 4, 'multiple_choice', NULL, '2018-03-16 08:49:25'),
-(2, 2, 4, 'multiple_choice', NULL, '2018-03-16 08:49:25'),
-(1, 1, 2, 'multiple_choice', NULL, '2018-03-16 08:50:56'),
-(1, 1, 3, 'multiple_choice', NULL, '2018-03-16 04:00:59');
 
 -- --------------------------------------------------------
 
@@ -88,13 +75,6 @@ CREATE TABLE `class_course_section` (
   `location` varchar(200) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `class_course_section`
---
-
-INSERT INTO `class_course_section` (`class_id`, `title`, `description`, `offering`, `location`) VALUES
-(1, 'CMPSC462', 'Data Structures', 'Fall', 'Olmsted');
-
 -- --------------------------------------------------------
 
 --
@@ -106,17 +86,6 @@ CREATE TABLE `owns_question` (
   `teacher_id` bigint(20) NOT NULL,
   `question_id` bigint(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `owns_question`
---
-
-INSERT INTO `owns_question` (`teacher_id`, `question_id`) VALUES
-(1, 1),
-(1, 2),
-(1, 3),
-(1, 4),
-(2, 5);
 
 -- --------------------------------------------------------
 
@@ -130,14 +99,6 @@ CREATE TABLE `owns_question_set` (
   `teacher_id` bigint(20) DEFAULT NULL,
   `question_set_id` bigint(20) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `owns_question_set`
---
-
-INSERT INTO `owns_question_set` (`id`, `teacher_id`, `question_set_id`) VALUES
-(1, 1, 1),
-(2, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -154,17 +115,6 @@ CREATE TABLE `question` (
   `correct_answers` blob
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `question`
---
-
-INSERT INTO `question` (`question_id`, `type`, `description`, `potential_answers`, `correct_answers`) VALUES
-(1, 'multiple_choice', 'What does that say?', NULL, NULL),
-(2, 'multiple_choice', 'What is a stack?', NULL, NULL),
-(3, 'multiple_choice', 'What does optimal mean?', NULL, NULL),
-(4, 'multiple_choice', 'What does onPause() do?', NULL, NULL),
-(5, 'multiple_choice', 'What is the transitive property?', NULL, NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -178,16 +128,6 @@ CREATE TABLE `question_history` (
   `session_id` bigint(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `question_history`
---
-
-INSERT INTO `question_history` (`id`, `question_id`, `session_id`) VALUES
-(4, 3, 2),
-(3, 1, 2),
-(2, 4, 1),
-(1, 2, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -199,16 +139,6 @@ CREATE TABLE `question_is_in` (
   `question_set_id` bigint(20) NOT NULL,
   `question_id` bigint(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `question_is_in`
---
-
-INSERT INTO `question_is_in` (`question_set_id`, `question_id`) VALUES
-(1, 1),
-(1, 2),
-(2, 3),
-(2, 4);
 
 -- --------------------------------------------------------
 
@@ -226,14 +156,6 @@ CREATE TABLE `question_session` (
   `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `question_session`
---
-
-INSERT INTO `question_session` (`id`, `class_id`, `question_set_id`, `start`, `end`, `date`) VALUES
-(1, 1, 1, '2018-03-16 04:00:00', '2018-03-17 04:00:00', '2018-03-16 04:00:00'),
-(2, 1, 2, '2018-03-30 04:00:00', '2018-03-31 04:00:00', '2018-03-30 04:00:00');
-
 -- --------------------------------------------------------
 
 --
@@ -245,14 +167,6 @@ CREATE TABLE `question_set` (
   `question_set_id` bigint(20) NOT NULL,
   `name` varchar(200) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `question_set`
---
-
-INSERT INTO `question_set` (`question_set_id`, `name`) VALUES
-(1, '462 Question Set 1'),
-(2, '462 Question Set 2');
 
 -- --------------------------------------------------------
 
@@ -272,15 +186,6 @@ CREATE TABLE `student_account` (
   `uniqueID` varchar(128) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `student_account`
---
-
-INSERT INTO `student_account` (`student_id`, `email`, `first_name`, `last_name`, `is_confirmed`, `salted_password`, `title`, `uniqueID`) VALUES
-(1, 'hexi@gmail.com', 'Legendary', 'Hexi', 1, 'HEXISALTEDPASS', 'student', 'HEXIEMAILID'),
-(2, 'basketball4lyf@gmail.com', 'Big', 'Chris', 1, 'BIGCHRISPASS', 'baller', 'BIGCHRISEMAILID'),
-(3, 'kraken@gmail.com', 'The', 'Kraken', 0, 'KRAKENPASS', NULL, 'KRAKENEMAILID');
-
 -- --------------------------------------------------------
 
 --
@@ -292,15 +197,6 @@ CREATE TABLE `student_is_in` (
   `student_id` bigint(20) NOT NULL,
   `class_id` bigint(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `student_is_in`
---
-
-INSERT INTO `student_is_in` (`student_id`, `class_id`) VALUES
-(1, 1),
-(2, 1),
-(3, 1);
 
 -- --------------------------------------------------------
 
@@ -320,14 +216,6 @@ CREATE TABLE `teacher_account` (
   `uniqueID` varchar(128) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `teacher_account`
---
-
-INSERT INTO `teacher_account` (`teacher_id`, `email`, `first_name`, `last_name`, `is_confirmed`, `salted_password`, `title`, `uniqueID`) VALUES
-(1, 'blum@psu.edu', 'Jeremy', 'Blum', 1, 'JEREMYBLUMSALTEDPASSWORD', NULL, 'JEREMYBLUMEMAILID'),
-(2, 'Omar.El.Ariss@tamuc.edu', 'Omar', 'El Ariss', 1, 'OMARSALTEDPASSWORD', 'Professor', 'OMAREMAILID');
-
 -- --------------------------------------------------------
 
 --
@@ -340,12 +228,17 @@ CREATE TABLE `teaches` (
   `teacher_id` bigint(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `teaches`
+-- Table structure for table `test`
 --
 
-INSERT INTO `teaches` (`class_id`, `teacher_id`) VALUES
-(1, 1);
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE `test` (
+  `id` varchar(2000) DEFAULT NULL,
+  `name` varchar(2000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -355,19 +248,23 @@ INSERT INTO `teaches` (`class_id`, `teacher_id`) VALUES
 -- Indexes for table `active_question`
 --
 ALTER TABLE `active_question`
-  ADD PRIMARY KEY (`question_set_id`,`question_id`);
+  ADD PRIMARY KEY (`question_set_id`,`question_id`),
+  ADD KEY `FK_active_question_question_id` (`question_id`);
 
 --
 -- Indexes for table `active_question_set`
 --
 ALTER TABLE `active_question_set`
-  ADD PRIMARY KEY (`class_id`,`question_set_id`);
+  ADD PRIMARY KEY (`class_id`,`question_set_id`),
+  ADD KEY `FK_active_question_set_question_set_id` (`question_set_id`);
 
 --
 -- Indexes for table `answers`
 --
 ALTER TABLE `answers`
-  ADD PRIMARY KEY (`student_id`,`sesssion_id`,`question_history_id`);
+  ADD PRIMARY KEY (`student_id`,`session_id`,`question_history_id`),
+  ADD KEY `FK_answers_session_id` (`session_id`),
+  ADD KEY `FK_answers_question_history_id` (`question_history_id`);
 
 --
 -- Indexes for table `class_course_section`
@@ -379,13 +276,16 @@ ALTER TABLE `class_course_section`
 -- Indexes for table `owns_question`
 --
 ALTER TABLE `owns_question`
-  ADD PRIMARY KEY (`teacher_id`,`question_id`);
+  ADD PRIMARY KEY (`teacher_id`,`question_id`),
+  ADD KEY `FK_owns_question_question_id` (`question_id`);
 
 --
 -- Indexes for table `owns_question_set`
 --
 ALTER TABLE `owns_question_set`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_owns_question_set_teacher_id` (`teacher_id`),
+  ADD KEY `FK_owns_question_set_question_set_id` (`question_set_id`);
 
 --
 -- Indexes for table `question`
@@ -397,19 +297,24 @@ ALTER TABLE `question`
 -- Indexes for table `question_history`
 --
 ALTER TABLE `question_history`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_question_history_question_id` (`question_id`),
+  ADD KEY `FK_question_history_session_id` (`session_id`);
 
 --
 -- Indexes for table `question_is_in`
 --
 ALTER TABLE `question_is_in`
-  ADD PRIMARY KEY (`question_set_id`,`question_id`);
+  ADD PRIMARY KEY (`question_set_id`,`question_id`),
+  ADD KEY `FK_question_is_in_question_id` (`question_id`);
 
 --
 -- Indexes for table `question_session`
 --
 ALTER TABLE `question_session`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_question_session_class_id` (`class_id`),
+  ADD KEY `FK_question_session_question_set_id` (`question_set_id`);
 
 --
 -- Indexes for table `question_set`
@@ -428,19 +333,22 @@ ALTER TABLE `student_account`
 -- Indexes for table `student_is_in`
 --
 ALTER TABLE `student_is_in`
-  ADD PRIMARY KEY (`student_id`,`class_id`);
+  ADD PRIMARY KEY (`student_id`,`class_id`),
+  ADD KEY `FK_student_is_in_class_id` (`class_id`);
 
 --
 -- Indexes for table `teacher_account`
 --
 ALTER TABLE `teacher_account`
-  ADD PRIMARY KEY (`teacher_id`);
+  ADD PRIMARY KEY (`teacher_id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `teaches`
 --
 ALTER TABLE `teaches`
-  ADD PRIMARY KEY (`class_id`,`teacher_id`);
+  ADD PRIMARY KEY (`class_id`,`teacher_id`),
+  ADD KEY `FK_teacher_teacher_id` (`teacher_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -450,42 +358,42 @@ ALTER TABLE `teaches`
 -- AUTO_INCREMENT for table `class_course_section`
 --
 ALTER TABLE `class_course_section`
-  MODIFY `class_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `class_id` bigint(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `owns_question_set`
 --
 ALTER TABLE `owns_question_set`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `question_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `question_id` bigint(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `question_history`
 --
 ALTER TABLE `question_history`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `question_session`
 --
 ALTER TABLE `question_session`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `question_set`
 --
 ALTER TABLE `question_set`
-  MODIFY `question_set_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `question_set_id` bigint(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `student_account`
 --
 ALTER TABLE `student_account`
-  MODIFY `student_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `student_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `teacher_account`
 --
 ALTER TABLE `teacher_account`
-  MODIFY `teacher_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `teacher_id` bigint(20) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
