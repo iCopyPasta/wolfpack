a9c06752101dce8261949493bc6db77362294248
Scott Wilson <kandabar@hotmail.com>
2018-03-20 22:03:39 -0400

    update dbCreate.sql

--- dbCreation/dbCreate.sql
+++ dbCreation/dbCreate.sql
@@ -8,6 +8,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+CREATE DATABASE IF NOT EXISTS `alpha_wolfpack` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `alpha_wolfpack`;
 
 DROP TABLE IF EXISTS `active_question`;
 CREATE TABLE IF NOT EXISTS `active_question` (
