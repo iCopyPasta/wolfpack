9ec544fd0aa3ad2d668602939c51e4a001ae3612
Tyler <sacapuntas9@gmail.com>
2018-03-20 22:08:23 -0400

    added modal box for add class, unwired

--- pages/logged_in_teacher.php
+++ pages/logged_in_teacher.php
@@ -24,7 +24,7 @@
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <link rel="stylesheet" type="text/css" href="..\css\special\modalcss.css" media="screen" />  
 
     <!-- Custom styles for this template -->
     <style>
@@ -70,8 +70,57 @@
       
         <a style="text-decoration: none" href ="#"> <button class="btn btn-info btn-lg btn-block" type="submit">Manage My Questions</button></a><br>
         <a style="text-decoration: none" href ="#"> <button class="btn btn-info btn-lg btn-block" type="submit">Manage My Question Sets</button></a><br>
-        <a style="text-decoration: none" href ="#"> <button href="#" class="btn btn-info btn-lg btn-block" type="submit"> Create New Class</button></a>
+        <a style="text-decoration: none"> <button id="createClassButton" class="btn btn-info btn-lg btn-block" type="submit">Create New Class</button></a>
     </div>
+      
+      <div id="myModal" class="modal">
+
+          <!-- Modal content -->
+          <div class="modal-content">
+            <div class="modal-header">
+                <h2>Create a Class</h2>
+              <span class="close">&times;</span>
+              
+            </div>
+            <div class="modal-body">
+              <p>Some text in the Modal Body</p>
+              <p>Some other text...</p>
+            </div>
+
+          </div>
+
+    </div>
+      
+      <script>
+        // Get the modal
+        var modal = document.getElementById('myModal');
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("createClassButton");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+      </script>
+      
+      
+      
 
     <div class="container">
     <h1 class="display-5 text-center">Current Classes</h1>
