64bc15a5d7197ef27a3bb7f99a4ee74cde7bcd32
Pablo Orellana <peo5032@psu.edu>
2018-03-21 00:40:11 -0400

    add searchClassesByTitleAndName which returns a paginted result list

--- pages/searchClassesByTitleAndName.php
+++ pages/searchClassesByTitleAndName.php
@@ -2,12 +2,14 @@
 
     /*
     
-    returns a paginated list of all the classes a student belongs to
+    returns a paginated list of all the classes for a search based on the teacher's name and class title
 
     expected POST variables:
         "currentPage" - the current page
         "rowsPerPage" - rows per page
-        "student_id - students id number
+        "title" - the title of class_course_section
+        "firstName" - the first name of the teacher
+        "lastName" - the last name of the teacher
         
     return: JSON obj containing a list of classes the student belongs to
 
@@ -15,26 +17,29 @@
     ensure you are using POST variables for the expected post variables
 
     Example Return JSON Value:
-    {"totalPages":1,
-    "totalResults":"6",
-    "success":1,
-    "results":[
-    {"class_id":"13",
-    "title":"Sed pariatur laboriosam perferendis sed sint.",
-    "description":"Rerum voluptas in quisquam nisi natus. Accusamus ut perferendis reiciendis quasi neque provident voluptatem. Quia ut repudiandae assumenda qui.",
-    "offering":"2014-09-07",
-    "location":"804 Valentin Coves Suite 389\nSouth William, GA 25690"}
-    ,
     {
-    "class_id":"38",
-    "title":"Aut mollitia dolore dolore voluptatem nam culpa error.",
-    "description":"Veritatis eos eius ut ad et. Ipsam nostrum consequatur libero sit minima sunt. Illo ullam velit laudantium quia iusto. Praesentium voluptatem dolorem fugiat est omnis ullam aut nostrum.",
-    "offering":"2004-10-18",
-    "location":"9526 Harris Prairie\nMarcellusside, SD 19327-7976"
-    }
-    ]}
-    
-
+    "totalPages": 30,
+    "totalResults": "300",
+    "success": 1,
+    "results": [{
+        "class_id": "3",
+        "title": "Repellat velit est ut saepe.",
+        "description": "Velit quis sint laudantium officiis velit voluptatum. Soluta assumenda consequuntur ut unde voluptas molestias sed repellendus. Molestias aut ut voluptas.",
+        "offering": "1983-02-16",
+        "location": "65336 Turner Springs\nPourosborough, FL 49915"
+    }, {
+        "class_id": "3",
+        "title": "Repellat velit est ut saepe.",
+        "description": "Velit quis sint laudantium officiis velit voluptatum. Soluta assumenda consequuntur ut unde voluptas molestias sed repellendus. Molestias aut ut voluptas.",
+        "offering": "1983-02-16",
+        "location": "65336 Turner Springs\nPourosborough, FL 49915"
+    }, {
+        "class_id": "5",
+        "title": "Ex modi et aut.",
+        "description": "Sed perferendis ad velit consequuntur dignissimos impedit vero ut. Autem ab nulla eum. Asperiores vel dolor ut blanditiis officia magnam. Soluta nobis soluta accusamus doloribus repudiandae.",
+        "offering": "1971-05-20",
+        "location": "5343 Ezequiel Square\nSouth Oswaldomouth, VT 64119-9317"
+    }]}
     */
 
     //default values for important variables for pagination
@@ -42,13 +47,15 @@
     $rowsPerPage = 10;
     $totalPages = 1;
     $currentPage = 1;
-    $student_id = 1;
-    
+
+
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         //in an ideal world, we'd perform sanitation!
         
         $rowsPerPage = isset($_POST["rowsPerPage"]) ? (int) $_POST["rowsPerPage"] : $rowsPerPage;
-        $student_id = isset($_POST["student_id"]) ? (int) $_POST["student_id"] : $student_id;
+        $firstName = isset($_POST["firstName"]) ? $_POST["firstName"] : "%";
+        $lastName = isset($_POST["lastName"]) ? $_POST["lastName"] : "%";
+        $title = isset($_POST["title"]) ? $_POST["title"] : "%";
         
         // get the current page or set a default
         $currentPage = isset($_POST["currentPage"])? (int) $_POST["currentPage"] : $currentPage;
@@ -59,11 +66,16 @@
         
         // find out how many rows are in the table
         $sql = "SELECT COUNT(*)
-                FROM student_is_in, class_course_section
-                WHERE :student_id = student_is_in.student_id
-                AND student_is_in.class_id = class_course_section.class_id";
+                FROM class_course_section, teaches, teacher_account
+                WHERE teacher_account.first_name LIKE :firstName
+                AND teacher_account.last_name LIKE :lastName
+                AND class_course_section.title LIKE :title
+                AND class_course_section.class_id = teaches.class_id
+                AND teaches.teacher_id = teacher_account.teacher_id";
         $result = $pdo->prepare($sql);
-        $result->bindValue(':student_id', $student_id);
+        $result->bindValue(':firstName', $firstName);
+        $result->bindValue(':lastName', $lastName);
+        $result->bindValue(':title', $title);
         
         try {
             $result->execute();
@@ -100,18 +112,18 @@
         
         // get the info from the db
 
-        $sql = "SELECT  class_course_section.class_id, 
-                        class_course_section.title, 
-                        class_course_section.description,
-                        class_course_section.offering,
-                        class_course_section.location
-                        
-                FROM student_is_in, class_course_section
-                WHERE :student_id = student_is_in.student_id
-                AND student_is_in.class_id = class_course_section.class_id";
-        
+        $sql = "SELECT class_course_section.class_id, class_course_section.title,
+        class_course_section.description, class_course_section.offering, class_course_section.location
+                FROM class_course_section, teaches, teacher_account
+                WHERE teacher_account.first_name LIKE :firstName
+                AND teacher_account.last_name LIKE :lastName
+                AND class_course_section.title LIKE :title
+                AND class_course_section.class_id = teaches.class_id
+                AND teaches.teacher_id = teacher_account.teacher_id";
         $result = $pdo->prepare($sql);
-        $result->bindValue(':student_id', $student_id);
+        $result->bindValue(':firstName', $firstName);
+        $result->bindValue(':lastName', $lastName);
+        $result->bindValue(':title', $title);
         
         try {
             $result->execute();
