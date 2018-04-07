217f865ade0165087185cd3a8b1643c77d4534f7
Scott Wilson <kandabar@hotmail.com>
2018-04-07 14:16:21 -0400

    bugfix liveStatistics.php
    now returns array of correct size

--- lib/php/liveStatistics.php
+++ lib/php/liveStatistics.php
@@ -23,7 +23,7 @@
 
   //ensure $session_id is populated
   $session_id = isset($_POST['session_id']) ? $_POST['session_id'] : null;
-  if(is_null($session_id) || empty($session_id)){
+  if (is_null($session_id) || empty($session_id)) {
     // fail JSON response
     $response = array();
     $response["message"] = "ERROR, session_id cannot be null or empty";
@@ -33,7 +33,7 @@
 
   //ensure $question_history_id is populated
   $question_history_id = isset($_POST['question_history_id']) ? $_POST['question_history_id'] : null;
-  if(is_null($question_history_id) || empty($question_history_id)){
+  if (is_null($question_history_id) || empty($question_history_id)) {
     // fail JSON response
     $response = array();
     $response["message"] = "ERROR, question_history_id cannot be null or empty";
@@ -41,9 +41,51 @@
     return json_encode($response);
   }
 
+  //get the count of potential_question field from question table
+  $sql = "SELECT DISTINCT potential_answers
+                FROM question, question_history, answers
+                WHERE answers.question_history_id = question_history.id
+                  AND question_history.question_id = question.question_id
+                  AND answers.question_history_id = :question_history_id
+          ";
+
+  $connection = new Connection;
+  $pdo = $connection->getConnection();
+  $stmt = $pdo->prepare($sql);
+  $stmt->bindValue(':question_history_id', $question_history_id);
+
+  try {
+    $stmt->execute();
+  } catch (Exception $e) {
+    // fail JSON response
+    $response = array();
+    $response["message"] = "ERROR SELECTING: " . $e->getMessage();
+    $response["success"] = 0;
+    return json_encode($response);
+  }
+
+  // success selecting potential_answers
+  $selectPotentialAnswers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  // get the count of potential_answers
+  $countPotentialAnswers = count(json_decode($selectPotentialAnswers[0]['potential_answers']));
+
+  // fail if count == 0; there are no potential_answers
+  if($countPotentialAnswers == 0) {
+    $response = array();
+    $response["message"] = "ERROR count of potential answers cannot be 0";
+    $response["success"] = 0;
+    return json_encode($response);
+  }
+
+  //initialize retVal array to 0
+  for($i = 0; $i < $countPotentialAnswers; $i++){
+    $retVal[$i] = 0;
+  }
+
   //find live statistics
-  $question = new Answers('%', $session_id, $question_history_id, '%', '%', '%');
-  $jsonVal = json_decode($question->select());
+  $answers = new Answers('%', $session_id, $question_history_id, '%', '%', '%');
+  $jsonVal = json_decode($answers->select());
   unset($jsonVal[0]); // remove response array
   foreach ($jsonVal as $key => $value) {
     $answerArray = (array)$value;  // cast object to associative array
@@ -54,19 +96,6 @@
       $retVal[$value2] = isset($retVal[$value2]) ? $retVal[$value2] + 1 : 0;
     }
   }
-
-  // sort array by keys; so keys are in order of {0th, 1st, 2nd, 3rd} instead of {1st, 3rd, 2nd, 0th}
-  ksort($retVal);
-
-  // fill in the gaps in the array
-  for ($i = 0; $i < sizeof($retVal); $i++) {
-    if (!isset($retVal[$i])) {
-      $retVal[$i] = 0;
-    }
-  }
-
-  // sort array by keys; so keys are in order of {0th, 1st, 2nd, 3rd} instead of {1st, 3rd, 2nd, 0th}
-  ksort($retVal);
 
   // success JSON response
   $response = array();
