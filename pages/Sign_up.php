e1d279c74833690835f4c519a0a33eba6027f3e1
Scott Wilson <kandabar@hotmail.com>
2018-04-15 18:07:21 -0400

    removed feedback Sign_up.php

--- pages/Sign_up.php
+++ pages/Sign_up.php
@@ -71,7 +71,7 @@
             $response = array();
             $response["message"] = "ERROR: password doesn't match confirmPassword";
             $response["success"] = 0;
-            echo json_encode($response);
+            return json_encode($response);
           }
         }
       }
@@ -90,7 +90,7 @@
           $response = array();
           $response["message"] = "ERROR: email already exists";
           $response["success"] = 0;
-          echo json_encode($response);
+          return json_encode($response);
         }
       }
     } // email,pw1,pw2 null
@@ -108,7 +108,7 @@
         $response = array();
         $response["message"] = "ERROR: fields cannot be null";
         $response["success"] = 0;
-        echo json_encode($response);
+//        echo json_encode($response);
         $matchPWString = '<div class="alert alert-danger">
                         <strong>Error: </strong> Email and pw cannot be null
                         </div>';
